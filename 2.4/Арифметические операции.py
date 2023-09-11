375530ce3c0c1771ea29765acc0f041b817ee044
Lev <106928252+Quanitte@users.noreply.github.com>
2023-09-11 09:28:09 +0300

    Delete 2.4 directory

--- 2.4/Арифметические операции.py
+++
@@ -1,2 +0,0 @@
-a, b = int(input()), int(input())
-print(f'{a} + {b} = {a + b}', f'{a} - {b} = {a - b}', f'{a} * {b} = {a * b}', sep='\n')
--- 2.4/Значение функции.py
+++
@@ -1,2 +0,0 @@
-a, b = int(input()), int(input())
-print(3 * ((a + b)**3) + (275 * b**2) - (127 * a) - 41)
